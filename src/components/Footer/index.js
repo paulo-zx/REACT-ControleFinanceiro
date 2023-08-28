c27b76ba73b1d309b1520b6808b72e19ee710dae
paulo-zx <paulo-zx@hotmail.com>
2023-08-28 20:59:12 -0300

    Controle Financeiro

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as C from "./styles"; 
+
+const Header = () => {
+  return (
+    <C.Container>
+        <C.Header>
+            <C.Title>Desenvolvido por Paulo Roberto Ribeiro Morais, 2023</C.Title>
+        </C.Header>
+    </C.Container>
+  )
+}
+
+export default Header
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,19 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    text-align: center;
+    background: teal;
+    font-size: 10px;
+    
+`;
+
+export const Header = styled.h2``;
+
+export const Title = styled.div`
+    
+    color: #fff;
+`; 
